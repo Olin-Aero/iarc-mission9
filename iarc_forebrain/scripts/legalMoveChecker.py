79b0d3138d27be21051d10cc013da6835b758212
nathanestill <nathan.estill@students.olin.edu>
2020-02-23 20:30:00 -0500

    adding new message types to only utilize one topic each

--- iarc_forebrain/scripts/legalMoveChecker.py
+++ iarc_forebrain/scripts/legalMoveChecker.py
@@ -7,49 +7,35 @@ class legalMoveChecker(object):
 	"""docstring for legalMoveChecker"""
 	def __init__(self):
 		rospy.init_node('legalMoveChecker', anonymous=True)
-		rospy.Subscriber("/req_initial",String,self.req_initialCB)
-		rospy.Subscriber("/req_destination",String,self.req_destinationCB)
-		rospy.Subscriber("/piece_initial",String,self.piece_initialCB)
-		rospy.Subscriber("/piece_destination",String,self.piece_destinationCB)
-		self.moveInitPub = rospy.Publisher("/move_initial",String,queue_size=10)
-		self.moveDestPub = rospy.Publisher("/move_destination",String,queue_size=10)
-		self.req_initial = None
-		self.req_destination = None
-		self.piece_initial = None
-		self.piece_destination = None
+		rospy.Subscriber("/req_move",SquareMove,self.requestedMoveCB)
+		self.moveSquarePub = rospy.Publisher("/move_square",SquareMove,queue_size=10)
+		self.move = None
 		self.rate = rospy.Rate(10)
 		
-	def req_initalCB(self,msg):
-		self.req_initial = msg.data
-
-	def req_destinationCB(self,msg):
-		self.req_destination = msg.data
-
-	def piece_initialCB(self,msg):
-		self.piece_initial = msg.data
-
-	def piece_destinationCB(self,msg):
-		self.piece_destination = msg.data
+	def requestedMoveCB(self,msg):
+		self.move = msg
 
 	def checkMove(self):
 		board = chess.Board("8/8/8/8/8/8/8/8")
-		executive = "board.set_piece_at(chess.{},chess.Piece(chess.{},chess.{}))".format(self.req_initial,self.piece_initial.type,self.piece_initial.color)
+		executive = "board.set_piece_at(chess.{},chess.Piece(chess.{},chess.{}))".format(self.move.init_square.data,self.move.init_piece.type.data,self.move.init_piece.color.data)
 		exec(executive)
-		executive = "board.set_piece_at(chess.{},chess.Piece(chess.{},chess.{}))".format(self.req_destination,self.piece_destination.type,self.piece_destination.color)
+		executive = "board.set_piece_at(chess.{},chess.Piece(chess.{},chess.{}))".format(self.move.dest_square.data,self.move.dest_piece.type.data,self.move.dest_piece.color.data)
 		if(chess.Move.from_uci((req_initial+req_destination).lower()) in board.legal_moves):
 			if(piece_destination == None):
-				self.move_initial.publish(self.req_initial)
-				self.move_destination.publish(self.req_destination)
+				self.toGo = self.move
+				self.toGo.capture.data = False
 			else:
-				self.move_initial.publish(self.req_destination)
-				self.move_destination.publish("TRASH")
-				self.move_initial.publish(self.req_initial)
-				self.move_destination.publish(self.req_destination))
+				self.toGo = self.move
+				self.toGo.capture.data = True
+		else:
+			rospy.logerror("Illegal Move, try again.")
+		self.move = None
 
 
 	def main(self):
 		while not rospy.is_shutdown():
-			self.checkMove()
+			if(self.move != None):
+				self.checkMove()
 			self.rate.sleep()
 
 
