b6cb6081ee1cd53593754f9e4910c75992c29145
nathanestill <nathan.estill@students.olin.edu>
2020-02-26 20:04:34 -0500

    adding the messages to this branch

--- iarc_forebrain/scripts/key2Square.py
+++ iarc_forebrain/scripts/key2Square.py
@@ -3,8 +3,9 @@
 import rospy
 from std_msgs.msg import String
 import re
+from iarc_forebrain.msg import MovePiece,Piece
 
-class keyboardInput():
+class key2Square():
 	"""
 	Takes input from a keyboard in the form of "A6 to B7" or "d2 -> e5" and 
 	converts it to an inital and destination of the requested move.
@@ -13,21 +14,22 @@ class keyboardInput():
 	"""
 	def __init__(self):
 		rospy.init_node('keyboardInput', anonymous=True)
-		self.initPub = rospy.Publisher('/req_initial',String,queue_size=10)
-		self.destPub = rospy.Publisher('/req_destination',String,queue_size=10)
+		self.requestPub = rospy.Publisher('/req_square',MovePiece,queue_size=10)
 
 	def run(self):
 		while not rospy.is_shutdown():
 			text = raw_input("Make a move (e.g. A5 to B6):")
 			squares = re.findall("[A-F]+[1-8]",text.upper())
 			if(len(squares) == 2):
-				self.initPub.publish(String(squares[0]))
-				self.destPub.publish(String(squares[1]))
+				request = MovePiece()
+				request.init_square = squares[0]
+				request.dest_square = squares[1]
+				self.requestPub.publish(request)
 			elif(len(squares) < 2):
 				rospy.loginfo("Not enough squares input or mistyped square")
 			elif(len(squares > 2)):
 				rospy.loginfo("Too many squares input")
 
 if __name__ == '__main__':
-	key = keyboardInput()
+	key = key2Square()
 	key.run()
